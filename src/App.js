7ebfe9b35f40b2a1acd50f6b427271dccdf5a44d
Namhehe237 <namnguyen23072003@gmail.com>
2025-01-03 20:53:08 +0700

    config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './layouts/navBarNFooter/Navbar';
 import Footer from './layouts/navBarNFooter/Footer';
-import Home from './layouts/Component/Home';
+import ProductListPage from './layouts/Component/ProductPage/ProductListPage';
+import HomePage from './layouts/Component/HomePage/HomePage';
+import CartPage from './layouts/Component/CartPage/CartPage';
+import { CartProvider } from './context/CartContext';
+
 
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
+    <CartProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path="/products" element={<ProductListPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            {/* Add more routes here if needed */}
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 };
-
 export default App;
